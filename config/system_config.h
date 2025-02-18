fb5e1867e2ebe0bd7b4ea0feeb603ad84acf7f25
mikha <mikhaelmiro300@gmail.com>
2025-02-18 12:20:28 +0100

    sys config

---
+++ config/system_config.h
@@ -0,0 +1,10 @@
+#ifndef SYSTEM_CONFIG_H
+#define SYSTEM_CONFIG_H
+
+#define SYSTEM_NAME "Missile Embedded System"
+#define VERSION "1.0"
+#define TELEMETRY_INTERVAL_MS 1000  // 1-second telemetry updates
+#define LOW_POWER_MODE_THRESHOLD 15.0  // Enable power-saving at 15% battery
+#define CRITICAL_SHUTDOWN_THRESHOLD 5.0
+
+#endif // SYSTEM_CONFIG_H
