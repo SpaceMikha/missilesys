cf7b6fdf4471c3a330ba695cf21ab35e82b89bd7
mikha <mikhaelmiro300@gmail.com>
2025-02-18 12:21:20 +0100

    hardware abstraction implemented

---
+++ firmware/include/hardware_abstraction.c
@@ -0,0 +1,17 @@
+#include "hardware_abstraction.h"
+#include <stdio.h>
+
+void configure_clock() {
+    printf("Configuring system clock...\n");
+    // Clock configuration logic
+}
+
+void initialize_gpio() {
+    printf("Initializing GPIO pins...\n");
+    // GPIO setup logic
+}
+
+void setup_peripherals() {
+    printf("Setting up peripherals (UART, SPI, I2C)...\n");
+    // Peripheral setup logic
+}
---
+++ firmware/include/hardware_abstraction.h
@@ -0,0 +1,8 @@
+#ifndef HARDWARE_ABSTRACTION_H
+#define HARDWARE_ABSTRACTION_H
+
+void configure_clock();
+void initialize_gpio();
+void setup_peripherals();
+
+#endif // HARDWARE_ABSTRACTION_H
